6b8b6e04c50de65c636ff249beeea4f617ca6045
SamuelWade <samuelrcwade@gmail.com>
2023-06-24 20:48:34 -0400

    lab completed

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,23 @@
-#!/usr/bin/env python3
-
 class MyString:
-  pass
+  def __init__(self,value=""):
+    self.value=value
+  def get_value(self):
+    return self._value
+  def set_value(self,newString):
+    if isinstance(newString,str):
+      self._value=newString
+    else:
+      print("The value must be a string.")
+  value=property(get_value,set_value)
+  def is_exclamation(self):
+    return self._value.endswith("!")
+  def is_sentence(self):
+    return self._value.endswith(".")
+  def is_question(self):
+    return self._value.endswith("?")
+  def count_sentences(self):
+    value=self.value
+    for punc in [".","!"]:
+      value=value.replace(punc,"?")
+    new_sentences = [s for s in value.split("?") if s]
+    return len(new_sentences)
